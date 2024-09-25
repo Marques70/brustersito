47727340fe6370f73ec7b1fa01d6d86909302352
Marques70 <foraneoip@hotmail.es>
2024-09-25 21:01:11 +0200

    guarreando el fichero

--- brus.py
+++ brus.py
@@ -2,3 +2,8 @@ print ("estamos en clase. que contento estoy con mi clase")
 print ("hooy salimos a las 9")
 print ("os lo habeis creido")
 print ("os lo digo diego")
+print print bla bla bla blña
+hgxdfhguloñaxdfhvgoñaxdfhvgoñaxdfoñv
+zxcvblñvfoñxdfhvoñhzxcoñvbjzxcǘ
+cvlkcvjbiñpzcvjbjcvb
+cvgxcgjb
